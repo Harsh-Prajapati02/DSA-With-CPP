caedde8615624979781c7e6fea655fbd51157d30
Harsh-Prajapati02 <harsh.prajapati0206@gmail.com>
2025-03-03 20:51:13 +0530

    update

--- Recursion_Exercises/sum_recursion.cpp
+++ Recursion_Exercises/sum_recursion.cpp
@@ -2,19 +2,19 @@
 using namespace std;
 
 // Function to calculate sum from 1 to N using recursion
-int sumToN(int n)
+int sum(int n)
 {
     if (n == 0)
         return 0;
-    return n + sumToN(n - 1);
+    return n + sum(n - 1);
 }
 
-int main()
+int main()  
 {
     int n;
     cout << "Enter a number: ";
     cin >> n;
 
-    cout << "Sum of numbers from 1 to " << n << " is: " << sumToN(n) << endl;
+    cout << "Sum of numbers from 1 to " << n << " is: " << sum(n) << endl;
     return 0;
 }
